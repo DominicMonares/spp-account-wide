ce105d04cd45a3c21ec46c54a650e14ca4c7f594
akaClay <domakaclay@gmail.com>
2022-08-20 02:18:26 -0700

    establish db connection

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
+// get all non-bot accounts
+// go through each account and:
+  // get each character guid
+  // create object to track achievements on account
+  // if timestamp of current achievement is older, replace in obj
+  // once every character's achievements have been added:
+    // go through every player again and for each player:
+      // go through achievements and check to see if current character has it
+      // add it if not
+
+// NEED TO ACCOUNT FOR FACTION SPECIFIC ACHIEVEMENTS
+
+const wotlkcharacters = mysql.createConnection({
   host: "127.0.0.1",
   port: 3310,
   user: "root",
@@ -8,13 +20,15 @@ const db = mysql.createConnection({
   database: "wotlkcharacters"
 });
 
-db.connect(err => {
-  if (err) { return res.sendStatus(400).send(err) };
-  console.log('Connected to DB!');
+wotlkcharacters.connect(err => {
+  if (err) console.error(err);
+  console.log('Connected to wotlkcharacters!');
 });
 
-const getCheevos = 'SELECT * FROM character_achievement WHERE guid=4501;';
-db.query(getCheevos, (err, data, fields) => {
+const charAchieves = 'SELECT * FROM character_achievement WHERE guid=10001;';
+wotlkcharacters.query(charAchieves, (err, data, fields) => {
   if (err) console.error(err);
   console.log('DATA ', data);
 });
+
+wotlkcharacters.end();
