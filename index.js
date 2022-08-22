ed8e2c52e071c270b7998f586a911ebbcef876da
akaClay <domakaclay@gmail.com>
2022-08-22 03:32:59 -0700

    add temporary invocation

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const { exec } = require('child_process');
 
 exec('start cmd.exe /K "node src/index.js & exit"');
+
+// REMOVE IMPORT AND INVOCATION FOR PRODUCTION
+const { transfer_achievements } = require('./src/index.js');
+transfer_achievements();
