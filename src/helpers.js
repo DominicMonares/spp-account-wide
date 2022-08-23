b84c6613380c88ecedade90db7400f27efc50470
akaClay <domakaclay@gmail.com>
2022-08-23 04:21:03 -0700

    implement faction

--- src/helpers.js
+++ src/helpers.js
@@ -26,9 +26,16 @@ const error = () => {
   );
 }
 
-
+const faction = (race) => {
+  if (race === 1 || race === 3 || race === 4 || race === 7 || race === 11) {
+    return 'A';
+  } else {
+    return 'H';
+  }
+}
 
 module.exports = { 
   closeWindow: closeWindow,
-  error: error
+  error: error,
+  faction: faction
 };
