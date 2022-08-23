12a8383777140d254de6e161feb454eef69a8603
akaClay <domakaclay@gmail.com>
2022-08-23 03:37:32 -0700

    relocate

---
+++ src/helpers.js
@@ -0,0 +1,34 @@
+const readline = require('readline');
+
+const closeWindow = (count) => {
+  setInterval(() => {
+    // console.log(`This window will close in ${count} seconds...`);
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(`This window will close in ${count - 1} seconds...`);
+    count--;
+
+    if (count <= 0) process.exit(0);
+  }, 1000);
+}
+
+const error = () => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  return new Promise(
+    () => rl.question(
+      'Press ENTER to close window.', 
+      () => process.exit(1)
+    )
+  );
+}
+
+
+
+module.exports = { 
+  closeWindow: closeWindow,
+  error: error
+};
