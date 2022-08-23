8a257681de632d95c34f7ebfa7d5c3eb82293b92
akaClay <domakaclay@gmail.com>
2022-08-23 02:46:34 -0700

    cleanup

--- src/queries/wotlkcharacters.js
+++ src/queries/wotlkcharacters.js
@@ -9,7 +9,7 @@ const getCharacters = async (accounts) => {
 
   try {
     wotlkcharacters = await mysql.createConnection(dbCredentials);
-    console.log('Connected to wotlkcharacters!');
+    console.log('Connected to wotlkcharacters...');
   } catch (err) {
     // native error msg printing after error func for some reason, revisit
     await error(err); 
@@ -20,6 +20,7 @@ const getCharacters = async (accounts) => {
     const accountQuery = `SELECT guid, account FROM characters WHERE account IN (${accountValues})`;
     let [rows] = await wotlkcharacters.execute(accountQuery);
     characters = rows;
+    console.log('Character data fetched...')
   } catch (err) {
     // native error msg printing after error func for some reason, revisit
     await error(err);
@@ -27,7 +28,7 @@ const getCharacters = async (accounts) => {
 
   try {
     await wotlkcharacters.end();
-    console.log('Disconnected from wotlkcharacters!')
+    console.log('Disconnected from wotlkcharacters...')
   } catch (err) {
     // native error msg printing after error func for some reason, revisit
     await error(err);
--- src/queries/wotlkrealmd.js
+++ src/queries/wotlkrealmd.js
@@ -9,7 +9,7 @@ const getAccounts = async () => {
 
   try {
     wotlkrealmd = await mysql.createConnection(dbCredentials);
-    console.log('Connected to wotlkrealmd!');
+    console.log('Connected to wotlkrealmd...');
   } catch (err) {
     // native error msg printing after error func for some reason, revisit
     await error(err); 
@@ -19,7 +19,7 @@ const getAccounts = async () => {
     const accountQuery = `SELECT id FROM account WHERE username NOT LIKE '%RNDBOT%'`;
     let [rows] = await wotlkrealmd.execute(accountQuery);
     accounts = rows;
-    console.log('Account data fetched!')
+    console.log('Account data fetched...')
   } catch (err) {
     // native error msg printing after error func for some reason, revisit
     await error(err);
@@ -27,7 +27,7 @@ const getAccounts = async () => {
 
   try {
     await wotlkrealmd.end();
-    console.log('Disconnected from wotlkrealmd!')
+    console.log('Disconnected from wotlkrealmd...')
   } catch (err) {
     // native error msg printing after error func for some reason, revisit
     await error(err);
