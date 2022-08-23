f69c8a168b80c5e1549f059e57efa95d7d176235
akaClay <domakaclay@gmail.com>
2022-08-23 01:39:04 -0700

    implement closeWindow

--- src/queries/helpers.js
+++ src/queries/helpers.js
@@ -1,5 +1,17 @@
 const readline = require('readline');
 
+const closeWindow = (count) => {
+  setInterval(() => {
+    // console.log(`This window will close in ${count} seconds...`);
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(`This window will close in ${count - 1} seconds...`);
+    count--;
+
+    if (count <= 0) process.exit(0);
+  }, 1000);
+}
+
 const error = () => {
   const rl = readline.createInterface({
     input: process.stdin,
@@ -14,4 +26,9 @@ const error = () => {
   );
 }
 
-module.exports = { error: error };
+
+
+module.exports = { 
+  closeWindow: closeWindow,
+  error: error
+};
