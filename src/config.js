f22a9557b15867c45a7d1fd97cb88232be4dd543
akaClay <domakaclay@gmail.com>
2022-08-23 01:53:42 -0700

    init

---
+++ src/config.js
@@ -0,0 +1,10 @@
+const dbCredentials = { // default SPP credentials
+  host: '127.0.0.1',
+  port: 3310,
+  user: 'root',
+  password: '123456'
+};
+
+module.exports = {
+  dbCredentials: dbCredentials
+}
