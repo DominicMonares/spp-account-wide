65e6865d34669496fa23f95a6240c5cfc06b912f
akaClay <domakaclay@gmail.com>
2022-08-25 02:29:04 -0700

    init

--- src/transfers/progress_transfer.js
+++ src/transfers/progress_transfer.js
@@ -7,6 +7,32 @@
   Loremaster? Will be complicated
 */
 
+/*
+Got My Mind On My Money
+50 Honorable Kills
+Mercilessly Dedicated
+Alterac Valley Veteran
+Arathi Basin Veteran
+Eye of the Storm Veteran
+Warsong Gulch Veteran
+Strand of the Ancients Veteran
+Wintergrasp Veteran
+Isle of Conquest Victory
+25 Dungeon & Raid Emblems
+Looking for Multitudes
+
+
+
+SEPARATE, MORE SPECIFIC
+Loremaster of Eastern Kingdoms
+Loremaster of Kalimdor
+50 Quests Complete -> batch with Loremaster logic
+5 Daily Quests Complete -> same logic as previous, different db queries
+Frostbitten
+Bloody Rare
+40 Exalted Reputations
+*/
+
 const transfer_progress = () => {
   console.log('Transfer progress called!')
 }
