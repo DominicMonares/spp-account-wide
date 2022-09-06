1479f3bc1fe82b18737033769a60a0a68899f687
akaClay <domakaclay@gmail.com>
2022-09-06 05:53:05 -0700

    entries

---
+++ src/data/progress.js
@@ -0,0 +1,166 @@
+// criteria - version - completion - achievement id
+const progress = {
+  1176: { // Got My Mind On My Money - 100g
+    criteria: 3506,
+    complete: 10000
+  },
+  1177: { // Got My Mind On My Money - 1,000g
+    criteria: 3507,
+    complete: 10000000
+  },
+  1178: { // Got My Mind On My Money - 5,000g
+    criteria: 3512,
+    complete: 50000000
+  },
+  1180: { // Got My Mind On My Money - 10,000g
+    criteria: 3510,
+    complete: 100000000
+  },
+  1181: { // Got My Mind On My Money - 25,000g
+    criteria: 3511,
+    complete: 250000000
+  },
+
+  513: { // 100 Honorable Kills
+    criteria: 6797,
+    complete: 100
+  },
+  515: { // 500 Honorable Kills
+    criteria: 6796,
+    complete: 500
+  },
+  516: { // 1000 Honorable Kills
+    criteria: 6795,
+    complete: 1000
+  },
+  512: { // 5000 Honorable Kills
+    criteria: 6794,
+    complete: 5000
+  },
+  509: { // 10000 Honorable Kills
+    criteria: 6793,
+    complete: 10000
+  },
+  239: { // 25000 Honorable Kills
+    criteria: 6792,
+    complete: 25000
+  },
+  869: { // 50000 Honorable Kills
+    criteria: 6791,
+    complete: 50000
+  },
+  870: { // 100000 Honorable Kills
+    criteria: 6790,
+    complete: 100000
+  },
+
+  398: { // Mercilessly Dedicated
+    criteria: 307,
+    complete: 100
+  },
+  875: { // Vengefully Dedicated
+    criteria: 1829,
+    complete: 200
+  },
+  876: { // Brutally Dedicated
+    criteria: 1830,
+    complete: 300
+  },
+
+  219: { // Alterac Valley Veteran
+    criteria: 225,
+    complete: 100
+  },
+  155: { // Arathi Basin Veteran
+    criteria: 176,
+    complete: 100
+  },
+  209: { // Eye of the Storm Veteran
+    criteria: 223,
+    complete: 100
+  },
+  167: { // Warsong Gulch Veteran
+    criteria: 221,
+    complete: 100
+  },
+  1309: { // Strand of the Ancients Veteran
+    criteria: 4502,
+    complete: 100
+  },
+
+  3839: { // 25 Dungeon & Raid Emblems
+    criteria: 11556,
+    complete: 25
+  },
+
+/*
+// Wintergrasp Veteran
+100 - 1718
+///// criteria /////
+
+// Isle of Conquest Victory
+100 - 3777
+///// criteria /////
+
+// 25 Dungeon & Raid Emblems
+
+50 - 3840
+100 - 3841
+250 - 3842
+500 - 3843
+1000 - 3844
+1500 - 3876
+2500 - 4316
+///// criteria /////
+4950
+4952
+4955
+6140
+6141
+6142
+11601
+11605
+11609
+11613
+11617
+11621
+12421
+
+// Looking for Multitudes
+10 - 4476
+50 - 4477
+100 - 4478
+///// criteria /////
+
+// 5 Daily Quests Complete
+5 - 973
+50 - 974
+200 - 975
+500 - 976
+1000 - 977
+///// criteria /////
+
+// 50 Quests Complete
+50 - 503
+100 - 504
+250 - 505
+500 - 506
+1000 - 507
+1500 - 508
+2000 - 32
+3000 - 978
+///// criteria /////
+
+SEPARATE, MORE SPECIFIC
+Loremaster of Eastern Kingdoms
+Loremaster of Kalimdor
+
+*/
+};
+
+module.exports = {
+  progress: progress
+}
+
+// 426
+// 427
